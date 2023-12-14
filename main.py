1eb843a5a5716be49ce660f02360f49a2335bd60
Minivalen <153239695+Minivalen@users.noreply.github.com>
2023-12-14 17:41:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import random
-danielito=["#matate daniel", "eso tilin"]
-print (danielito[0])
-tilin=input("decime algo canchero")
-print("matate",tilin)
+from turtle import *
+import colorsys
+danielito={"daniel":"eso tilin"}
+tilin= input("decime daniel")
+if tilin in danielito.keys():
+    print (danielito[tilin])
+else:
+    bgcolor("red")
+    pencolor("black")
+    print("maatate cabron")
+    title ("no me dijiste daniel :(")
+    texto=("matate cabron")
+    color("black")
+    penup()
+    goto(0,0)
+    pendown()
+    write(texto,align="left",font=("calibri",12,"normal"))
 
+    mainloop()
 
